b26610df32ddeec06e0dbf9df3774f97b2086586
zhangwenyu2 <zhangwy5@chinatowercom.cn>
2017-12-05 22:59:06 +0800

    Update

--- day_4/2_dml.sql
+++ day_4/2_dml.sql
@@ -11,7 +11,22 @@ INSERT INTO db_day04.employee (name, hiredate)
   VALUE ('name1', '2017-1-1'), ('name2', '2017-1-1'), ('name3', '2017-1-1');
 
 
+UPDATE db_day04.employee
+SET age = 19, gender = 'F'
+WHERE id = 2;
+
+UPDATE db_day04.employee
+SET gender = 'M'
+WHERE gender IS NULL;
+
+DELETE FROM db_day04.employee
+WHERE name LIKE '%name%';
+
 SELECT *
 FROM db_day04.employee;
 
+DROP DATABASE db_day04;
+
 SHOW DATABASES ;
+
+SHOW tables FROM scott;
